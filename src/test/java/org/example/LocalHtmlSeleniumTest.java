27d552464b560137d728e053ffb2a781e9e58210
Khushic-123 <khushic@lambdatest.com>
2025-10-07 16:15:33 +0530

    committed

--- src/test/java/org/example/LocalHtmlSeleniumTest.java
+++ src/test/java/org/example/LocalHtmlSeleniumTest.java
@@ -4,47 +4,59 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LocalHtmlSeleniumTest {
 
     @Test
     public void testLocalHtml() throws Exception {
-
-
+        // Credentials from environment
         String username = System.getenv("LT_USERNAME");
         String accessKey = System.getenv("LT_ACCESS_KEY");
+        if (username == null || accessKey == null) {
+            throw new IllegalStateException("Please set LT_USERNAME and LT_ACCESS_KEY in environment variables");
+        }
 
-        // General browser capabilities
-        MutableCapabilities capabilities = new MutableCapabilities();
-        capabilities.setCapability("browserName", "Chrome");
-        capabilities.setCapability("browserVersion", "latest");
+        // Local HTML page served on CircleCI container
+        String testUrl = "http://localhost:8080/index.html";
 
-        // LambdaTest-specific options
-        MutableCapabilities ltOptions = new MutableCapabilities();
-        ltOptions.setCapability("username", username);
-        ltOptions.setCapability("accessKey", accessKey);
-        ltOptions.setCapability("tunnel", true);
-        ltOptions.setCapability("tunnelName", "myLocalTunnel");
-        ltOptions.setCapability("w3c", true);
-        ltOptions.setCapability("build", "Local HTML Build");
-        ltOptions.setCapability("name", "Local HTML Test");
+        // LambdaTest hub URL
+        String hubUrl = "https://" + username + ":" + accessKey + "@hub.lambdatest.com/wd/hub";
 
-        // Attach LambdaTest options
-        capabilities.setCapability("LT:Options", ltOptions);
+        // Chrome browser options
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("browserName", "Chrome");
+        options.setCapability("browserVersion", "latest");
 
-        // Create RemoteWebDriver instance
-        RemoteWebDriver driver = new RemoteWebDriver(
-                new URL("https://hub.lambdatest.com/wd/hub"), capabilities
-        );
+        // LambdaTest options (W3C)
+        Map<String, Object> ltOptions = new HashMap<>();
+        ltOptions.put("platformName", "Windows 11");
+        ltOptions.put("tunnel", true);
+        ltOptions.put("tunnelName", "myLocalTunnel");
+        ltOptions.put("build", "CircleCI - LambdaTest Local HTML");
+        ltOptions.put("name", "Local HTML Test");
+        ltOptions.put("w3c", true);
 
-        // Open your local page via tunnel
-        driver.get("http://localhost:8080/index.html");
-        System.out.println("Page Title: " + driver.getTitle());
+        // Attach LT options
+        options.setCapability("LT:Options", ltOptions);
 
-        driver.quit();
+        WebDriver driver = new RemoteWebDriver(new URL(hubUrl), options);
+
+        try {
+            driver.get(testUrl);
+            String title = driver.getTitle();
+            System.out.println("Page title: " + title);
+            assertTrue(title != null && title.length() > 0);
+        } finally {
+            driver.quit();
+        }
     }
 }
